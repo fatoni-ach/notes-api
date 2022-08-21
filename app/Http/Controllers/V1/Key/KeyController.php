e89da8d3a1a1010b3b57dcf55de7163d235cf16f
fatoni-ach <fatoni.noah@gmail.com>
2022-08-21 23:26:50 +0700

    scenario register key

--- app/Http/Controllers/V1/Key/KeyController.php
+++ app/Http/Controllers/V1/Key/KeyController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers\V1\Key;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\KeyRequest;
-use App\Models\Note;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class KeyController extends Controller
 {
     public function registerKey(KeyRequest $request)
     {
-        $validated = $request-validated();
-        return response()->json([
-            'register key'
+        $validated = $request->validated();
+
+        $user = User::create([
+            'name'  => $validated['name'],
+            'email' => $validated['email'],
+            'password'  => Hash::make($validated['password']),
+            'secret_key' => Str::random(20),
         ]);
+
+        return response()->json([
+            'status'    => 'success'
+        ], 200);
     }
 }
